06464bb0956a560e14a1e83ced5570a3ca61ecb5
kaelzhang <i@kael.me>
2014-05-28 00:32:51 +0800

    removes unnecessary lines

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ var through = require('through2');
 var node_path = require('path');
 
 var file = node_path.join(__dirname, 'jade', 'common.jade');
-var common = fs.readFileSync(file);
+var common = fs.readFileSync(file).toString();
 
-module.exports = function (options){
-  options = options || {};
+module.exports = function (){
   return through.obj(function (file, enc, callback) {
-    options.filename = file.path;
-    // file.path = handleExtension(file.path, opts);
-
     if(file.isStream()){
-      this.emit('error', new PluginError('gulp-jade', 'Streaming not supported'));
+      this.emit('error', new PluginError('gulp-cortex-jade-mixins', 'Streaming not supported'));
       return callback();
     }
 
